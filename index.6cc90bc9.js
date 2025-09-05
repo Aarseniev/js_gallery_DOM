9a6f2cda5821bed8ea08e0a5e6f26ea8744b7e9f
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-06 01:26:32 +0300

    make build

---
+++ index.6cc90bc9.js
@@ -0,0 +1,2 @@
+var e,t;e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg"),e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");r&&t&&(t.src=r.href)});
+//# sourceMappingURL=index.6cc90bc9.js.map
